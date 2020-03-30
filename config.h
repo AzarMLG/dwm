7eabbf76c623767d8d1684ef7335e60729ae02e2
Artyom Azarevich <temafilt@gmail.com>
2020-03-30 17:35:15 +0300

    Start dmenu by ctrl+p

--- config.h
+++ config.h
@@ -68,7 +68,7 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-    { MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+    { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
     { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },                   /*Toggle bar*/
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
