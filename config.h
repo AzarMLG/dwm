8295527fd901eb402d1afca1adf870a057410115
Artyom Azarevich <temafilt@gmail.com>
2020-04-14 19:45:00 +0300

    new border size & gaps between window, new color scheme

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx       = 4;   /* border pixel of windows */
-static const unsigned int gappx          = 7;   /* gaps between windows */
+static const unsigned int borderpx       = 5;   /* border pixel of windows */
+static const unsigned int gappx          = 5;   /* gaps between windows */
 static const unsigned int snap           = 16;  /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X*/
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -14,16 +14,16 @@ static const int topbar                  = 1;   /* 0 means bottom bar */
 static const char *fonts[]               = { "monospace:size=10" };
 static const char dmenufont[]            = "monospace:size=10";
 
-static const char col_gray1[]            = "#222222";
-static const char col_gray2[]            = "#444444";
-static const char col_gray3[]            = "#bbbbbb";
-static const char col_gray4[]            = "#eeeeee";
-static const char col_cyan[]             = "#005577";
+static const char col_background[]       = "#282A36";
+static const char col_darkgray[]         = "#BFBFBF";
+static const char col_lightgray[]        = "#E6E6E6";
+static const char col_foreground[]       = "#F8F8F2";
+static const char col_red[]              = "#FF5555";
 
 static const char *colors[][3]           = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_lightgray, col_background, col_darkgray },
+	[SchemeSel]  = { col_foreground, col_red,  col_red  },
 };
 
 /* tagging */
@@ -67,7 +67,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_background, "-nf", col_lightgray, "-sb", col_red, "-sf", col_foreground, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
